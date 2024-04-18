3447ef890661d982c1eec26c05b5b56eae48b544
Sophie Johnson <66137461+smj2173@users.noreply.github.com>
2024-04-18 14:36:33 -0400

    Add files via upload

---
+++ train.py
@@ -0,0 +1,131 @@
+import torchvision
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torchvision import transforms,models,datasets
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import os
+from torch import optim
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+import cv2, glob, numpy as np, pandas as pd
+import matplotlib.pyplot as plt
+from glob import glob
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+from random import shuffle, seed
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.ticker as mtick
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+
+
+#!pip install torch_summary
+#from torchsummary import summary
+
+def get_images(folder):
+    dictionary = dict()
+    directories = ['dog','fish']
+    for d in directories:
+        for item in os.listdir(folder + "/" + d):
+            if item.split(".")[-1] in ('png', 'jpg'): #this is mapping file name to label
+                dictionary[d] = item
+    return dictionary
+
+def get_model():
+    model = models.resnet18(pretrained=True)
+    for param in model.parameters():
+        param.requires_grad = False
+    model.avgpool = nn.AdaptiveAvgPool2d(output_size=(1,1))
+    model.fc = nn.Sequential(nn.Flatten(),
+    nn.Linear(512, 128),
+    nn.ReLU(),
+    nn.Dropout(0.2),
+    nn.Linear(128, 1),
+    nn.Sigmoid())
+    loss_fn = nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr= 1e-3)
+    return model.to(device), loss_fn, optimizer
+
+def train_batch(x, y, model, opt, loss_fn):
+    model.train()
+    prediction = model(x)
+    batch_loss = loss_fn(prediction, y)
+    batch_loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+    return batch_loss.item()
+
+@torch.no_grad()
+def accuracy(x, y, model):
+    model.eval()
+    prediction = model(x)
+    is_correct = (prediction > 0.5) == y
+    return is_correct.cpu().numpy().tolist()
+
+def get_data():
+    train = get_images(os.getcwd() + '/imagenette2/train/')
+    #trn_dl = DataLoader(train, batch_size=32, shuffle=True, drop_last = True)
+    val = get_images(os.getcwd() + '/imagenette2/val/')
+    #val_dl = DataLoader(val, batch_size=32, shuffle=True, drop_last = True)
+    return train, val
+
+trn_dl, val_dl = get_data()
+model, loss_fn, optimizer = get_model()
+
+train_losses, train_accuracies = [], []
+val_accuracies = []
+
+print("All losses and accuracies are for each epoch")
+for epoch in range(5):
+    
+    train_epoch_losses, train_epoch_accuracies = [], []
+    val_epoch_accuracies = []
+
+    for ix, batch in trn_dl.items():
+        x = batch
+        y = ix
+        batch_loss = train_batch(x, y, model, optimizer, loss_fn)
+        train_epoch_losses.append(batch_loss) 
+    train_epoch_loss = np.array(train_epoch_losses).mean()
+
+    for ix, batch in trn_dl.items():
+        x = batch
+        y = ix
+        is_correct = accuracy(x, y, model)
+        train_epoch_accuracies.extend(is_correct)
+    train_epoch_accuracy = np.mean(train_epoch_accuracies)
+    
+    for ix, batch in val_dl.items():
+        x = batch
+        y = ix
+        val_is_correct = accuracy(x, y, model)
+        val_epoch_accuracies.extend(val_is_correct)
+    val_epoch_accuracy = np.mean(val_epoch_accuracies)
+
+    print(f" epoch {epoch + 1}/5, Training Loss: {train_epoch_loss}, Training Accuracy: {train_epoch_accuracy}, Validation Accuracy: {val_epoch_accuracy}")
+    train_losses.append(train_epoch_loss)
+    train_accuracies.append(train_epoch_accuracy)
+    val_accuracies.append(val_epoch_accuracy)
+    
+    epochs = np.arange(5)+1
+
+plt.plot(epochs, train_accuracies, 'b', label='Training accuracy')
+plt.plot(epochs, val_accuracies, 'r', label='Validation accuracy')
+plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
+plt.title('Training and validation accuracy with ResNet18 \nand 1K training data points')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.ylim(0.95,1)
+plt.gca().set_yticklabels(['{:.0f}%'.format(x*100) for x in plt.gca().get_yticks()]) 
+plt.legend()
+plt.grid('off')
+plt.show()
+
+
+    
+    
+
+
+
