6a9028154da050a5ca19685c876d1dec13b65509
nathankoenig <nak2172@columbia.edu>
2024-04-21 10:29:12 -0400

    organizing

--- server.py
+++ server.py
@@ -1,40 +1,52 @@
-# -*- coding: utf-8 -*-
-import numpy as np
-import tensorflow as tf
-import PIL.Image as img
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--model", type=str, required=True)
-args = parser.parse_args()
-
-model = tf.keras.models.load_model(args.model, compile=False)
-model._make_predict_function()
-session = tf.keras.backend.get_session()
+from torchvision import models, transforms
+from PIL import Image
+import torch
 
 app = Flask(__name__)
 
+
+
+
+# TODO: Have to add .device(cuda) or something like that at the end of some statements for it to run on the gpu
+
+
+
+
+# Load the PyTorch models
+imagenet = torch.load('model1.pth')
+damagenet = torch.load('model2.pth')
+
+# Page without image uploaded
 @app.route('/')
 def index():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    if request.method == 'POST':
-        f = request.files.get('file')
-        fname = secure_filename(f.filename)
-        f.save(fname)
+# Page with image uploaded
+@app.route('/classify', methods=['POST'])
+def classify():
 
-        data = np.array([np.array(img.open(fname).convert('L').resize((28, 28), img.ANTIALIAS))])
-        data = np.expand_dims(data, -1) / 255.0
+    # Error check if file exists
+    if 'image' not in request.files:
+        return "No image found in request!"
 
-        with session.as_default():
-            predict = model.predict(data)
-            predict = np.argmax(predict[0])
+    # Error check if image was selected
+    image_file = request.files['image']
+    if image_file.filename == '':
+        return "No image selected!"
 
-        return render_template('predict.html', predict=str(predict))
+    # Open image and get model outputs (classifications)
+    # TODO: May have to preprocess the iamge?
+    image = Image.open(image_file)
+    imagenet_pred = model1(image)
+    damagenet_pred = model2(image)
+    
+    # Print out the outputs
+    result_html = f"<p>Model 1 Classification: {output1}</p>"
+    result_html += f"<p>Model 2 Classification: {output2}</p>"
+    
+    # Render the new html
+    return result_html
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
